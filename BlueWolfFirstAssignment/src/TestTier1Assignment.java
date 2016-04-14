4d46e4a56ca63ee9634ddc2858f66d3eccfe573e
Devon McBride <devonmcb@yahoo.com>
2016-04-14 00:24:53 -0400

    Finished interview test code for BlueWolf.

--- BlueWolfFirstAssignment/src/TestTier1Assignment.java
+++ BlueWolfFirstAssignment/src/TestTier1Assignment.java
@@ -7,7 +7,7 @@ import java.util.Random;
 /**
  * @author devonmcb@yahoo.com Devon McBride
  * 
- * Return work to michael.goldstein@bluewolfgroup.com
+ * Return to michael.goldstein@bluewolfgroup.com
  * 
  * Assumptions: 
  * 1. Since this is a "quick and easy" exercise, I am assuming the domain of
@@ -20,37 +20,115 @@ public class TestTier1Assignment {
 
 	public static void main(String args[]){
 	
-		Tier1Assignment assignmentObject = new Tier1Assignment();		
-		assignmentObject.speak();
-		
-		//Largest String can be length Integer.MAX_VALUE
-//		String str = null; 
-//		Random randy = new Random();
-		// MAX_VALUE on my machine is 2147483647, but was too big. 
-		//int big = Integer.MAX_VALUE/2;
-//		for(int x = 0; x < big; x++){
-//			str += (char)randy.nextInt(255);
-//		}
-		
-		//String str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-		//System.out.println(str);
-	/*good tests
-		System.out.println(assignmentObject.getFirstUniqueChar("234523452351")); //1
-		System.out.println(assignmentObject.getFirstUniqueChar("345976873456009832ohgo8tryuo6tuoijlfujt  . . . . ururur%%%%")); //2
-		System.out.println(assignmentObject.getFirstUniqueChar("abbashhsryyrttttttttttttt3tttttttttttt")); //3
-		System.out.println(assignmentObject.getFirstUniqueChar("")); // fail
-		System.out.println(assignmentObject.getFirstUniqueChar("abba")); // fail
-		System.out.println(assignmentObject.getFirstUniqueChar("a")); // a
-	*/
-		int[] theArray = new int[1000000];
-		for (int i=0; i<1000000; i++){
-			theArray[i] = i;
-		}
+		Tier1Assignment theObj = new Tier1Assignment();		
 
-		System.out.println(assignmentObject.findTheDuplicateNumber(theArray)); // -1
-		theArray[888888] = 999999;
-		System.out.println(assignmentObject.findTheDuplicateNumber(theArray)); // -1
+		/****************************************************************
+		 ****************************************************************
+		 * 	TEST FUNCTION getFirstUniqueChar()
+		 ****************************************************************
+		 ****************************************************************/
+		System.out.println("The following are testing getFirstUniqueChar().............................");
 		
+
+		/********************************
+		 * tests expected to pass
+		 ********************************/
+		System.out.println("getFirstUniqueChar() returned: " + theObj.getFirstUniqueChar("DEFD") + ", E expected");		
+		System.out.println("getFirstUniqueChar() returned: " + theObj.getFirstUniqueChar("234523452351") + ", 1 expected"); //1
+		System.out.println("getFirstUniqueChar() returned: " + theObj.getFirstUniqueChar("ABABABA12134567!@#$") + ", 2 expected"); //2
+		System.out.println("getFirstUniqueChar() returned: " + theObj.getFirstUniqueChar("abbashhstttttttttt3tttshhs12") + ", 3 expected"); //3
+		System.out.println("getFirstUniqueChar() returned: " + theObj.getFirstUniqueChar("a") + ", a expected"); // a
+		String str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		System.out.println("getFirstUniqueChar() returned: " + theObj.getFirstUniqueChar(str) + ", L expected");
+		
+		/********************************
+		 * tests expected to fail
+		 ********************************/
+		System.out.println();
+		System.out.println("The next tests of getFirstUniqueChar() are expected to return null -- causing string in test to think it's ended.");
+		System.out.println("getFirstUniqueChar() returned: " + theObj.getFirstUniqueChar("") + ", NULL character expected (will print nothing)"); // fail
+		System.out.println("getFirstUniqueChar() returned: " + theObj.getFirstUniqueChar("abba") + ", NULL character expected (will print nothing)"); // fail
+
+
+		/****************************************************************
+		 ****************************************************************
+		 * 	TEST FUNCTION findDupNum()
+		 ****************************************************************
+		 ****************************************************************/
+		System.out.println();
+		System.out.println("The following are testing findDupNum().............................");
+		
+		// set up array according to specification
+		int[] inOrderArray = new int[1000000];
+		for (int i=0; i<1000000; i++){
+			inOrderArray[i] = i+1;
+			// NOTE: e.g., index 0 has value 1; index 999999 has value 1,000,000
+		}
+		
+		/********************************
+		 * tests expected to pass
+		 ********************************/
+		// boundary test: 1
+		inOrderArray[888888] = 1; // should fail, b.c. 0 isn't between 1 and 1,000,000
+		System.out.println("Expecting: 1, got: " + theObj.findDupNum(inOrderArray));
+		inOrderArray[888888] = 888888+1; // 'reset' that one
+
+		// boundary test: 1000000
+		inOrderArray[888888] = 1000000; // should fail, b.c. 0 isn't between 1 and 1,000,000
+		System.out.println("Expecting: 1000000, got: " + theObj.findDupNum(inOrderArray));
+		inOrderArray[888888] = 888888+1; // 'reset' that one
+
+		// arbitrary duplicate
+		inOrderArray[888888] = 12345;
+		System.out.println("Expecting: 12345, got: " + theObj.findDupNum(inOrderArray));
+		inOrderArray[888888] = 888888+1; // 'reset' that one
+		
+		// some simple randoms just for kicks
+		Random randy = new Random();
+		for (int i=0; i<3; i++){
+			int testValue = randy.nextInt(1000000);
+			int testIndex = randy.nextInt(1000000);
+			while(testValue == testIndex){ // ensure these two are different
+				testValue = randy.nextInt(1000000);
+				testIndex = randy.nextInt(1000000);
+			}
+			inOrderArray[testIndex] = testValue;
+			System.out.println("Expecting: " + testValue + ", got: " + theObj.findDupNum(inOrderArray));
+			inOrderArray[testIndex] = testIndex+1; // 'reset' that one
+		}
+		
+		/********************************
+		 * tests expected to fail
+		 ********************************/
+
+		// boundary test: 0
+		inOrderArray[888888] = 0;
+		System.out.println("Expecting: -1, got: " + theObj.findDupNum(inOrderArray));
+		inOrderArray[888888] = 888888+1; // 'reset' that one
+
+		// boundary test: 1000001
+		inOrderArray[888888] = 1000001; 
+		System.out.println("Expecting: -1, got: " + theObj.findDupNum(inOrderArray));
+		inOrderArray[888888] = 888888+1; // 'reset' that one
+
+		// boundary test: -1
+		inOrderArray[888888] = -1; 
+		System.out.println("Expecting: -1, got: " + theObj.findDupNum(inOrderArray));
+		inOrderArray[888888] = 888888+1; // 'reset' that one
+
+		// overflow test: Integer.MAX_VALUE
+		inOrderArray[888888] = Integer.MAX_VALUE + 1; 
+		System.out.println("Expecting: -1, got: " + theObj.findDupNum(inOrderArray));
+		inOrderArray[888888] = 888888+1; // 'reset' that one
+
+		// overflow test: Integer.MIN_VALUE
+		inOrderArray[888888] = Integer.MIN_VALUE - 1; 
+		System.out.println("Expecting: -1, got: " + theObj.findDupNum(inOrderArray));
+		inOrderArray[888888] = 888888+1; // 'reset' that one
+				
+		// empty (small) array
+		int[] smallEmptyArray = new int[1];
+		System.out.println("Expecting: -1, got: " + theObj.findDupNum(smallEmptyArray));
 		
 	
 	}
--- BlueWolfFirstAssignment/src/Tier1Assignment.java
+++ BlueWolfFirstAssignment/src/Tier1Assignment.java
@@ -2,59 +2,52 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * 
- */
-
-/**
  * @author devonmcb@yahoo.com Devon McBride
  * 
- * Return work to michael.goldstein@bluewolfgroup.com
+ * Return to michael.goldstein@bluewolfgroup.com
  * 
- * Assumptions: 
- * 1. Use Basic Multilingual Plane (BMP) of characters;
- * the set of characters from U+0000 to U+FFFF.
+ * Write a function in the programming language of your choice to solve the
+ * following problems (see each of the two function's comments). 
+ * Each function MUST be accompanied by positive and negative unit tests. 
  *
  */
 public class Tier1Assignment {
 	
 	/**
 	 * i.) Given an array with 1,000,000 integers between 1 and 1,000,000, one
-	 * integer is in the array twice. Find the duplicate. 
+	 * integer is in the array twice. Find the duplicate.
 	 * 
-	 * NOTE: when no non-repeating integer is found, 
-	 * the -1 is returned.
-	 * (Or, it could throw an exception, but that's ugly.) 
+	 * NOTE: Since I'm given (i.e. 'guaranteed') an array as described above, 
+	 * I am not testing for such things as multiple duplicates.  
+	 * In my driver, however, I have provided some tests beyond this spec. 
+	 * 
+	 * NOTE DESIGN CHOICE: -1 is returned when no non-repeating integer is found.
+	 * NOTE DESIGN CHOICE: -1 is ALSO returned when there is a number in the array 
+	 * which is not within the boundaries of the specification.
 	 */
-	public int findTheDuplicateNumber(int a[]){
-		
+	public int findDupNum(int a[]){
+				
 		HashSet<Integer> theSet = new HashSet<Integer>();
 		
-
 		for (int i=0; i<a.length; i++){
-			if(!theSet.add(a[i])){ // add returns false if already in set
+			if( 1 > a[i] || 1000000 < a[i] ) {
+				return -1; // must be between 1 and 1,000,000, else violates contract with this function
+			}
+			if(!theSet.add(a[i])){ // .add will return false if already in set
 				return a[i];
 			}			
 		}
 		
-		//checked the whole thing, and no duplicates, so return -1
-		return -1;
+		return -1; //checked the whole thing, and no duplicates, so return -1
 	}
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	
 	/**
 	 * ii.) Find the first non-repeating character in a string: ("DEFD" -> E)
 	 * 
-	 * NOTE: when no non-repeating character is found, 
-	 * the NULL character is returned.
-	 * (Or, it could throw an exception, but that's ugly.) 
+	 * NOTE DESIGN CHOICE: when no non-repeating character is found, the NULL character is returned.
+	 * (Or, it could have instead throw an exception.) 
 	 */
 	// Two loops, so O(n), which isn't bad. 
 	public char getFirstUniqueChar(String str) {
@@ -80,14 +73,10 @@ public class Tier1Assignment {
 				return c;
 			}
 		}
+		
 		int noUniques = 0;
 		return (char) noUniques;
 		
 		//throw new RuntimeException("No unique (non-repeating) character found.");
 	}
-	
-	public void speak() {
-		System.out.println("woof");
-	}
-
 }
